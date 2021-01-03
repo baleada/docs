fa4c8477b41ee146597ca884d7e4ac27584f6913
AlexVipond <alex.r.vipond@gmail.com>
2021-01-03 10:20:37 -0600

    gets dev version working

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import sourceTransformProseToVueSfc from './source-transforms/proseToVueSfc.js'
 import proseFilesToManifest from './source-transforms/proseFilesToManifest.js'
 import proseFilesToSearchableCandidates from './source-transforms/proseFilesToSearchableCandidates.js'
 
+import vue from '@vitejs/plugin-vue'
+import resolve from '@rollup/plugin-node-resolve'
+
 const testRoute = ({ source, id }) => {
   // Exclude dotfiles
   if (testable().include('**/.**').test(id)) {
@@ -14,61 +17,40 @@ const testRoute = ({ source, id }) => {
   return publish && id.endsWith('.md')
 }
 
-export default configureable('vite')
-  .alias({
-    '@components': '/src/components',
-    '@functions': '/src/functions',
-    '@state': '/src/state',
-  })
-  .koa(koa => koa
-    .asVue({
-      toVue: sourceTransformProseToVueSfc,
-      test: ({ id }) => id.endsWith('.md'),
+export default {
+  ...configureable('vite')
+    .alias({
+      '@components': '/src/components',
+      '@functions': '/src/functions',
+      '@state': '/src/state',
     })
-    .virtual.index('src/components')
-    .virtual.index('src/functions')
-    .virtual.index('src/state')
-    .virtual.routes({ path: 'src/prose/routes', router: 'vue' }, { test: testRoute, transformPath: path => path.replace(/^\//, '').replace(/\/index$/, '') })
-    .virtual({
-      test: testable().idEndsWith('src/state/manifest').test,
-      transform: () => ({
-        type: 'js',
-        source: proseFilesToManifest(),
-      }),
-    })
-    .virtual({
-      test: testable().idEndsWith('src/state/searchableCandidates').test,
-      transform: () => ({
-        type: 'js',
-        source: proseFilesToSearchableCandidates(),
-      }),
-    })
-    .configure()
-  )
-  .rollup(rollup => rollup
-    .sourceTransform({
-      transform: sourceTransformProseToVueSfc,
-      test: ({ id }) => id.endsWith('.md'),
-    })
-    .virtual.index('src/components')
-    .virtual.index('src/functions')
-    .virtual.index('src/state')
-    .virtual.routes({ path: 'src/prose/routes', router: 'vue' }, { test: testRoute, transformPath: path => path.replace(/^\//, '').replace(/\/index$/, '') })
-    .virtual({
-      test: testable().idEndsWith('src/state/manifest').test,
-      transform: () => proseFilesToManifest(),
-    })
-    .virtual({
-      test: testable().idEndsWith('src/state/searchableCandidates').test,
-      transform: () => proseFilesToSearchableCandidates(),
-    })
-    .configure()
-  )
-  .rollup.vue({
-    include: ['**/*.vue', '**/*.md'],
-  })
-  .includeDeps([
-    '@baleada/logic',
-  ])
-  .configure()
-
+    .includeDeps([
+      '@baleada/logic',
+    ])
+    .configure(),
+  plugins: [
+    resolve(),
+    ...configureable('rollup')
+      .sourceTransform({
+        transform: sourceTransformProseToVueSfc,
+        test: ({ id }) => id.endsWith('.md'),
+      })
+      .virtual.index('components')
+      .virtual.index('functions')
+      .virtual.index('state')
+      .virtual.routes({ path: 'prose/routes', router: 'vue' }, { test: testRoute, transformPath: path => path.replace(/^\//, '').replace(/\/index$/, '') })
+      .virtual({
+        test: testable().idEndsWith('src/state/manifest').test,
+        transform: () => proseFilesToManifest(),
+      })
+      .virtual({
+        test: testable().idEndsWith('src/state/searchableCandidates').test,
+        transform: () => proseFilesToSearchableCandidates(),
+      })
+      .configure()
+      .plugins,
+    vue({
+      include: ['**/*.vue', '**/*.md'],
+    }), 
+  ]
+}
