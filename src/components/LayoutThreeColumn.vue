d496a075e536584cb3739271747ba16027fc4f03
AlexVipond <alex.r.vipond@gmail.com>
2021-01-15 23:00:35 -0600

    debugging fullpath

--- src/components/LayoutThreeColumn.vue
+++ src/components/LayoutThreeColumn.vue
@@ -497,6 +497,11 @@ export default {
     const route = useRoute(),
           SITE_NAME = 'Baleada'
 
+    watch(
+      () => route.fullPath,
+      () => console.log(route.fullPath)
+    )
+
     useHead({
       title: computed(() => context.article.frontMatter?.title ?? SITE_NAME),
       metas: [
