d53a7053116e17dd4b10c2b7f3e1dca086ebf529
AlexVipond <alex.r.vipond@gmail.com>
2021-09-17 22:42:01 -0500

    vanity word count

--- source-transforms/proseFilesToManifest.ts
+++ source-transforms/proseFilesToManifest.ts
@@ -7,49 +7,60 @@ import fastGlob from 'fast-glob'
 export function proseFilesToManifest (): string {
   console.log('Creating manifest...')
 
+  let words: number = 0
+  const incrementWords = (increment: number) => (words += increment)
+
   const dirIds = fastGlob.sync('src/prose/**', { onlyDirectories: true }).sort(),
         manifest = [
           {
             level: 1,
             name: 'overview',
-            articles: toManifested('src/prose'),
+            articles: toManifested('src/prose', incrementWords),
           },
           ...dirIds.map(
             id => ({
               level: id.replace('src/prose/', '').split('/').length,
               name: parse(id).name.replace(/-/g, ' '),
-              articles: toManifested(id),
+              articles: toManifested(id, incrementWords),
             })
           )
         ].filter(({ articles }) => articles.length > 0)
 
   const totalArticles = manifest.reduce((totalArticles, { articles }) => totalArticles + articles.length, 0)
-  console.log(`${totalArticles} articles from ${manifest.length} directories added to the manifest`)
+  console.log(`${totalArticles} articles with ${new Intl.NumberFormat('en-US', { style: 'decimal' }).format(words)} words from ${manifest.length} directories added to the manifest`)
 
   return `export const manifest = ${JSON.stringify(manifest)}`
 }
 
-function toManifested (dirPath) {
+function toManifested (dirPath: string, incrementWords: (increment: number) => void) {
   const files = readdirSync(dirPath),
         manifestable = files.filter(file => (statSync(`${dirPath}/${file}`)).isFile()),
-        published = manifestable.filter(file => matter(readFileSync(`${dirPath}/${file}`, 'utf8')).data.publish === true),
-        manifested = published.map(
-          file => {
-            const { data: { title, tags: rawTags, order } } = matter(readFileSync(`${dirPath}/${file}`, 'utf8')),
-            tags = rawTags ? rawTags.split(',').map(tag => tag.trim()) : [],
-            fileName = parse(`${dirPath}/${file}`).name,
-            href = `/docs${dirPath.replace('src/prose', '')}${`/${fileName}`.replace(/^\/index$/, '')}`,
-            authorDate = (toStats(`${dirPath}/${file}`)).authorDate
+        manifested = manifestable.reduce((manifested, file) => {
+          const contents = readFileSync(`${dirPath}/${file}`, 'utf8'),
+                frontMatter = matter(contents)
 
-            return {
-              title,
-              tags,
-              href,
-              authorDate,
-              order,
-            }
+          incrementWords(frontMatter.content.split(' ').length)
+
+          if (!frontMatter.data.publish) {
+            return manifested
           }
-        )
+
+          const { data: { title, tags: rawTags, order } } = matter(readFileSync(`${dirPath}/${file}`, 'utf8')),
+                tags = rawTags ? rawTags.split(',').map(tag => tag.trim()) : [],
+                fileName = parse(`${dirPath}/${file}`).name,
+                href = `/docs${dirPath.replace('src/prose', '')}${`/${fileName}`.replace(/^\/index$/, '')}`,
+                authorDate = (toStats(`${dirPath}/${file}`)).authorDate
+
+          manifested.push({
+            title,
+            tags,
+            href,
+            authorDate,
+            order,
+          })
+
+          return manifested
+        }, [])
         
   return manifested.sort(byOrder)
 }
