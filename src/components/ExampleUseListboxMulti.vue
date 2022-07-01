263e02593aa7ff335f568a516d809c9bc9c44a7a
AlexVipond <alex.r.vipond@gmail.com>
2022-07-01 13:51:43 -0500

    fix styles and examples

--- src/components/ExampleUseListboxMulti.vue
+++ src/components/ExampleUseListboxMulti.vue
@@ -9,7 +9,7 @@
     <div
       :ref="listbox.root.ref"
       aria-label="Example listbox"
-      class="h-18 flex flex-col overflow-x-scroll"
+      class="h-24 flex flex-col overflow-x-scroll"
       :class="{
         'bg-white': store.statuses.darkTheme === 'disabled',
         'bg-primary-gray-90': store.statuses.darkTheme === 'enabled',
@@ -19,7 +19,7 @@
         v-for="(name, index) in organizations"
         :key="name"
         :ref="listbox.options.getRef(index)"
-        class="p-2"
+        class="p-2 focus:outline-none"
         :class="{
           'bg-primary-10 text-primary-90': listbox.is.focused(index) && !listbox.is.selected(index) && store.statuses.darkTheme === 'disabled',
           'bg-primary-50 text-primary-10': listbox.is.selected(index) && store.statuses.darkTheme === 'disabled',
@@ -33,7 +33,7 @@
     <section class="flex flex-col gap-4">
       <section class="flex flex-col gap-2">
         <label>Focused:</label>
-        <pre class="px-2 py-1 mt-2 mb-0"><code class="mr-auto">{{ listbox.focused }}</code></pre>
+        <pre class="px-2 py-1 mt-2 mb-0"><code class="mr-auto">{{ listbox.focused.location }}</code></pre>
       </section>
       <section class="flex flex-col gap-2">
         <label>Selected:</label>
@@ -57,7 +57,7 @@ export default {
     const organizations = ref(names.slice(Math.floor(names.length / 2)))
 
     const listbox = readonly(useListbox({ multiselectable: true })),
-          selectedJson = computed(() => JSON.stringify(listbox.selected, null, 2))
+          selectedJson = computed(() => JSON.stringify(listbox.selected.picks, null, 2))
           
     return {
       organizations,
--- src/components/ExampleUseListboxSingle.vue
+++ src/components/ExampleUseListboxSingle.vue
@@ -9,7 +9,7 @@
     <div
       :ref="listbox.root.ref"
       aria-label="Example listbox"
-      class="h-18 flex flex-col overflow-x-scroll shadow-5"
+      class="h-24 flex flex-col overflow-x-scroll shadow-5"
       :class="{
         'bg-white': store.statuses.darkTheme === 'disabled',
         'bg-primary-gray-90': store.statuses.darkTheme === 'enabled',
@@ -19,7 +19,7 @@
         v-for="(name, index) in organizations"
         :key="name"
         :ref="listbox.options.getRef(index)"
-        class="p-2"
+        class="p-2 focus:outline-none"
         :class="{
           'bg-primary-10 text-primary-90': listbox.is.focused(index) && !listbox.is.selected(index) && store.statuses.darkTheme === 'disabled',
           'bg-primary-50 text-primary-10': listbox.is.selected(index) && store.statuses.darkTheme === 'disabled',
@@ -33,11 +33,11 @@
     <section class="flex flex-col gap-4">
       <section class="flex flex-col gap-2">
         <label>Focused:</label>
-        <pre class="px-2 py-1 mt-2 mb-0"><code class="mr-auto">{{ listbox.focused }}</code></pre>
+        <pre class="px-2 py-1 mt-2 mb-0"><code class="mr-auto">{{ listbox.focused.location }}</code></pre>
       </section>
       <section class="flex flex-col gap-2">
         <label>Selected:</label>
-        <pre class="px-2 py-1 mt-2 mb-0"><code class="mr-auto">{{ listbox.selected ?? 'undefined' }}</code></pre>
+        <pre class="px-2 py-1 mt-2 mb-0"><code class="mr-auto">{{ listbox.selected.newest }}</code></pre>
       </section>
     </section>
   </section>
